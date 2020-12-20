7dc026da696a60fd86c4d56331a9c64f5dab9d37
malyvsen <5940672+malyvsen@users.noreply.github.com>
2020-12-20 23:34:03 +0100

    test: fix typo in reuters test

--- test/test_data.py
+++ test/test_data.py
@@ -7,11 +7,11 @@ def test_reuters():
     assert len(data.reuters) > 0
     for document in data.reuters.documents:
         assert len(document) > 0
-        assert sum(document.word_counts.values()) <= len(document)
+        assert sum(document.word_count.values()) <= len(document)
         for word in document.words:
             if word.include:
                 assert len(word.lda_form) > 0
                 characters = set(word.original_form.lower())
                 assert characters & set(string.ascii_lowercase)
                 num_included_words += 1
-    assert sum(data.reuters.word_counts.values()) == num_included_words
+    assert sum(data.reuters.word_count.values()) == num_included_words
