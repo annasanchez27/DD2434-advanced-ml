7238dee5595b46a7c2222bfe646439868ac7fffe
miszkur <miszkurka.aga@gmail.com>
2020-12-23 12:12:08 +0100

    add perplexity

---
+++ lda/experiments/perplexity/__main__.py
@@ -0,0 +1,13 @@
+import argparse
+import os
+from lda import data
+from .utils import corpus_to_documents_with_topics
+from .classification import plot_classification_for_label
+
+
+def main():
+    print("hello")
+
+
+if __name__ == "__main__":
+    main()
