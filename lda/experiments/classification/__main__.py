29c3253276528097ff798bac1771487631287ad6
miszkur <miszkurka.aga@gmail.com>
2020-12-23 12:06:29 +0100

    test classification_for_label

--- lda/experiments/classification/__main__.py
+++ lda/experiments/classification/__main__.py
@@ -1,66 +1,12 @@
 import argparse
-import numpy as np
-import matplotlib.pyplot as plt
 import os
 from lda import data
-from pathlib import Path
-from sklearn.model_selection import train_test_split
-from sklearn.svm import SVC
-from sklearn.metrics import accuracy_score
 from .utils import corpus_to_documents_with_topics
-from .utils import topic_to_labels
-from .utils import docs_to_features
+from .classification import plot_classification_for_label
 
 
 DATA_DIR = os.path.join(os.path.dirname(__file__),
                         '..', 'data', 'classification')
-
-
-def classification(label_name, corpus, dest_dir, test_size=0.8, random_state=42):
-    label_filename = label_name + '_labels.npy'
-    label_file = Path(os.path.join(dest_dir, label_filename))
-    if not label_file.is_file():
-        y = topic_to_labels(label_name, corpus)
-        np.save(os.path.join(dest_dir, label_filename), y)
-    else:
-        y = np.load(label_file)
-
-    X_train_docs, X_test_docs, y_train, y_test = train_test_split(
-        corpus, y, test_size=test_size, random_state=random_state)
-    X_train, X_test = docs_to_features(X_train_docs, X_test_docs, test_size)
-
-    SVM = SVC()
-    SVM.fit(X_train, y_train)
-    y_pred = SVM.predict(X_test)
-    return accuracy_score(y_pred, y_test)*100
-
-
-def classification_for_label(corpus, label_name, show_fig, dest_dir, seed_num):
-    train_data_fractions = [0.01, 0.05, 0.1, 0.2]
-    y = []
-    yerr = []
-    for train_frac in train_data_fractions:
-        accuracies = []
-        for random_state in range(seed_num):
-            acc = classification(label_name, corpus, dest_dir,
-                                 test_size=1-train_frac, random_state=random_state)
-            accuracies.append(acc)
-
-        y.append(np.mean(accuracies))
-        yerr.append(np.var(accuracies))
-        print("Accuracy for training fraction ",
-              train_frac, ":", np.mean(accuracies))
-
-    plt.figure()
-    plt.errorbar(train_data_fractions, y, yerr=yerr, label='Word features')
-    plt.title('Classification accuracy for ' + label_name + ' label')
-    plt.xlabel('Proportion of data used for training')
-    plt.ylabel('Accuracy')
-    plt.legend()
-    fig = plt.gcf()
-    fig.savefig(os.path.join(dest_dir, label_name))
-    if show_fig:
-        plt.show()
 
 
 def main():
@@ -78,7 +24,7 @@ def main():
     corpus = corpus_to_documents_with_topics(reuters_corpus)
 
     print(args)
-    classification_for_label(
+    plot_classification_for_label(
         corpus, args.label, args.show_fig, DATA_DIR, args.seeds)
 
 
--- test/experiments/classification/test_classification.py
+++ test/experiments/classification/test_classification.py
@@ -1,86 +1,41 @@
 from lda.data.document import Document
 from lda.data.corpus import Corpus
 from lda.data.word import Word
+from lda import data
 import lda.experiments.classification.utils as utils
+import lda.experiments.classification.classification as classification
 import numpy as np
 
-
-def create_word(original, include=True):
-    return Word(original, original.lower(), include=include)
+test_data_fractions = [0.99, 0.95, 0.9, 0.8]
 
 
-def create_mary_corpus():
-    mary = create_word("Mary")
-    had = create_word("had", include=False)
-    a = create_word("a", include=False)
-    little = create_word("little")
-    Little = create_word("Little")
-    lamb = create_word("lamb")
-    return Document([mary, had, a, little, lamb, Little, lamb, little, lamb], topics=['nursery_rhyme', 'lamb'])
+def test_classification_output_grain():
+    expected_accuracies = {
+        0.99: 94.44228148724937,
+        0.95: 94.53291408864996,
+        0.9: 95.62098501070663,
+        0.8: 97.37412671645387,
+    }
+    reuters_corpus = data.reuters
+    corpus = utils.corpus_to_documents_with_topics(reuters_corpus)
+
+    for test_frac in test_data_fractions:
+        accuracy = classification.classification_for_label(
+            corpus, "grain", '.', test_size=test_frac, save_labels=False)
+        assert accuracy == expected_accuracies[test_frac]
 
 
-def create_itsy_corpus():
-    Itsy = create_word("Itsy")
-    bitsy = create_word("bitsy")
-    spider = create_word("spider")
-    return Document([Itsy, bitsy, spider], topics=['nursery_rhyme'])
+def test_classification_output_earn():
+    expected_accuracies = {
+        0.99: 91.91162156900916,
+        0.95: 96.6528045440714,
+        0.9: 97.44111349036403,
+        0.8: 97.71139484461575,
+    }
+    reuters_corpus = data.reuters
+    corpus = utils.corpus_to_documents_with_topics(reuters_corpus)
 
-
-def create_lorem_corpus():
-    Lorem = create_word("Lorem")
-    ipsum = create_word("ipsum")
-    dot = create_word(".", include=False)
-    return Document([Lorem, ipsum, dot], topics=['placeholder'])
-
-
-def create_corpus_without_topics():
-    word1 = create_word("word1")
-    word2 = create_word("word2")
-    return Document([word1, word1, word2])
-
-
-def create_corpus_with_empty_topics():
-    word1 = create_word("word1")
-    word2 = create_word("word2")
-    return Document([word1, word1, word2], topics=[])
-
-
-def create_corpus():
-    doc1 = create_mary_corpus()
-    doc2 = create_itsy_corpus()
-    doc3 = create_lorem_corpus()
-    doc_without_topic = create_corpus_without_topics()
-    doc_with_empty_topic = create_corpus_with_empty_topics()
-    return Corpus([doc1, doc2, doc3, doc_without_topic, doc_with_empty_topic])
-
-
-def create_corpus_with_topics():
-    doc1 = create_mary_corpus()
-    doc2 = create_itsy_corpus()
-    doc3 = create_lorem_corpus()
-    return Corpus([doc1, doc2, doc3])
-
-
-def test_corpus_with_topics():
-    corpus = create_corpus()
-    expected_docs_with_topics = create_corpus_with_topics().documents
-
-    docs_with_topics = utils.corpus_to_documents_with_topics(corpus)
-
-    assert len(docs_with_topics) == len(expected_docs_with_topics)
-    # for i, doc in enumerate(docs_with_topics):
-    #   assert doc == expected_docs_with_topics[i]
-
-
-def test_topic_to_labels():
-    docs_with_topics = create_corpus_with_topics().documents
-    labels = utils.topic_to_labels("nursery_rhyme", docs_with_topics)
-    assert np.array_equal(np.array([1, 1, 0]), labels)
-
-
-def test_docs_to_wordcount_dicts():
-    expected = [{'mary': 1, 'little': 3, 'lamb': 3}, {
-        'itsy': 1, 'bitsy': 1, 'spider': 1}, {'lorem': 1, 'ipsum': 1}]
-    docs_with_topics = create_corpus_with_topics().documents
-    wordcount_dicts = utils.docs_to_wordcount_dicts(docs_with_topics)
-    assert expected == wordcount_dicts
+    for test_frac in test_data_fractions:
+        accuracy = classification.classification_for_label(
+            corpus, "earn", '.', test_size=test_frac, save_labels=False)
+        assert accuracy == expected_accuracies[test_frac]
