4d1c81e1c95fc0953109011313c1895163ff84eb
AleixEF <aleix.fontcuberta@gmail.com>
2020-12-29 19:56:04 +0100

    computing the lower bound of a document

--- lda/variational/lda.py
+++ lda/variational/lda.py
@@ -3,7 +3,7 @@ from tqdm.auto import trange
 from lda.data.corpus import Corpus
 from .e_step import e_step
 from .m_step import m_step
-
+from scipy.special import loggamma, digamma
 
 def lda(corpus: Corpus, num_topics=64, num_iterations=1024):
     '''
@@ -59,6 +59,18 @@ def lda(corpus: Corpus, num_topics=64, num_iterations=1024):
         ))
     return params
 
+"Eq. 15 on the paper. Lower bound to maximize for a document"
+def document_lower_bound(alpha, phi, gamma):
+    lower_bound = loggamma(np.sum(alpha)) - np.sum(loggamma(alpha))
+    lower_bound += np.sum((alpha-1)*(digamma(gamma)-digamma(np.sum(gamma))))
+    for n in range(phi.shape[0]):
+        lower_bound += np.sum(phi[n, :] * (digamma(gamma)-digamma(np.sum(gamma))))
+    # term with three sums to calculate
+    
+    lower_bound += -loggamma(np.sum(gamma)) + np.sum(loggamma(gamma))
+    lower_bound -= np.sum((gamma-1)*(digamma(gamma)-digamma(np.sum(gamma))))
+    lower_bound -= np.sum(phi * np.log(phi))
+    return lower_bound
 
 def random_categorical_distribution(num_choices):
     unnormalized = np.random.uniform(size=num_choices)
