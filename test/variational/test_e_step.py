58ff2ac1f941e96820c24bb5c213128120cc7cd3
malyvsen <5940672+malyvsen@users.noreply.github.com>
2020-12-30 12:31:21 +0100

    fix: do e_step after m_step

--- test/variational/test_e_step.py
+++ test/variational/test_e_step.py
@@ -1,10 +1,10 @@
 from lda.data.document import Document
 from lda.data.word import Word
-from lda.variational import e_step
+from lda.variational.e_step import e_step, document_e_step
 import numpy as np
 
 
-def test_output_shape():
+def test_document_output_shape():
     please = Word('Please', 'please', include=True)
     crash = Word('crash', 'crash', include=True)
     from_another_doc = Word('grandma', 'grandma', include=True)
@@ -22,6 +22,6 @@ def test_output_shape():
         {please: 0.7, crash: 0.2, from_another_doc: 0.1},
         {please: 0.2, crash: 0.6, from_another_doc: 0.2},
     ]
-    parameters = e_step(document=document, alpha=alpha, beta=beta)
+    parameters = document_e_step(document=document, alpha=alpha, beta=beta)
     assert parameters['phi'].shape == (2, 2)
     assert parameters['gamma'].shape == (2,)
