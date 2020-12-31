e60397693cb4ae280253abf8e01afdc579c15589
miszkur <miszkurka.aga@gmail.com>
2020-12-31 12:22:37 +0100

    add tqdm loading bars

--- experiments/classification/__main__.py
+++ experiments/classification/__main__.py
@@ -5,17 +5,11 @@ from .utils import corpus_to_documents_with_topics
 from .classification import plot_classification_for_label
 
 
-DATA_DIR = os.path.join(os.path.dirname(__file__),
-                        '..', 'data', 'classification')
-
-
 def main():
     parser = argparse.ArgumentParser(
         description='Parse arguments for classification.')
     parser.add_argument('--label', default='grain',
                         help='Classification label', type=str)
-    parser.add_argument('--show_fig', action='store_true',
-                        default=False, help='Show accuracy plot.')
     parser.add_argument(
         '--seeds', default=5, help='Number of different random seeds for train_test_split.')
     args = parser.parse_args()
@@ -24,8 +18,7 @@ def main():
     corpus = corpus_to_documents_with_topics(reuters_corpus)
 
     print(args)
-    plot_classification_for_label(
-        corpus, args.label, args.show_fig, DATA_DIR, args.seeds)
+    plot_classification_for_label(corpus, args.label, args.seeds)
 
 
 if __name__ == "__main__":
--- experiments/classification/classification.py
+++ experiments/classification/classification.py
@@ -5,8 +5,9 @@ from pathlib import Path
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score
-from .utils import topic_to_labels
-from .utils import docs_to_features
+from utils import topic_to_labels
+from utils import docs_to_features
+from tqdm.auto import tqdm
 
 
 def classification_for_label(documents, label_name, dest_dir, test_size=0.8, random_state=42, save_labels=True):
@@ -19,7 +20,6 @@ def classification_for_label(documents, label_name, dest_dir, test_size=0.8, ran
     else:
         y = np.load(label_file)
 
-    # print(len(documents))
     X_train_docs, X_test_docs, y_train, y_test = train_test_split(
         documents, y, test_size=test_size, random_state=random_state)
     X_train, X_test = docs_to_features(X_train_docs, X_test_docs, test_size)
@@ -30,20 +30,24 @@ def classification_for_label(documents, label_name, dest_dir, test_size=0.8, ran
     return accuracy_score(y_pred, y_test)*100
 
 
-def plot_classification_for_label(documents, label_name, show_fig, dest_dir, seed_num):
+DATA_DIR = os.path.join(os.path.dirname(__file__),
+                        '..', 'data', 'classification')
+
+
+def plot_classification_for_label(documents, label_name, seed_num=5):
     train_data_fractions = [0.01, 0.05, 0.1, 0.2]
     y = []
     yerr = []
-    for train_frac in train_data_fractions:
+    for iter, train_frac in enumerate(train_data_fractions):
         accuracies = []
-        for random_state in range(seed_num):
-            acc = classification_for_label(documents, label_name, dest_dir,
-                                 test_size=1-train_frac, random_state=random_state)
+        for random_state in tqdm(range(seed_num)):
+            acc = classification_for_label(documents, label_name, DATA_DIR,
+                                           test_size=1-train_frac, random_state=random_state)
             accuracies.append(acc)
 
         y.append(np.mean(accuracies))
         yerr.append(np.var(accuracies))
-        print(f'Accuracy for training fraction {train_frac}: {np.mean(accuracies)}')
+        print(f'[{iter+1}/{len(train_data_fractions)}] Accuracy for training fraction {train_frac}: {np.mean(accuracies)}')
 
     plt.figure()
     plt.errorbar(train_data_fractions, y, yerr=yerr, label='Word features')
@@ -51,7 +55,4 @@ def plot_classification_for_label(documents, label_name, show_fig, dest_dir, see
     plt.xlabel('Proportion of data used for training')
     plt.ylabel('Accuracy')
     plt.legend()
-    fig = plt.gcf()
-    fig.savefig(os.path.join(dest_dir, label_name))
-    if show_fig:
-        plt.show()
+    plt.show()
